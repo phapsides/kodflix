b05a1a8f2cc3d6594dbcdcedf397a7db94c248d7
Panos Hapsides <p.hapsides@live.com>
2020-03-02 15:47:06 +0000

    Add 6 movies and style in flexbox

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import './App.css';
 import lastSamurai from './the-last-samurai.jpg';
+import troy	from './troy.jpg'
+import marcoPolo from './marco-polo.jpg'
+import theLastKingdom from './the-last-kingdom.jpg'
+import blackDeath from './black-death.jpg'
+import ipMan from './ip-man.jpg'
 
 function App() {
   	return (
@@ -8,8 +13,26 @@ function App() {
 			<h1>
 				Welcome to Kodflix!
 			</h1>
-			<section className="favouriteMovie">
-				<img src={lastSamurai} alt="The last samurai cover" />
+			<section className="movieList">
+				
+				<div class="movie">
+					<img src={lastSamurai} alt="The last samurai cover" />
+				</div>
+				<div class="movie">
+					<img src={troy} alt="Troy cover" />
+				</div>
+				<div class="movie">
+					<img src={marcoPolo} alt="Marco Polo cover" />
+				</div>
+				<div class="movie">
+					<img src={theLastKingdom} alt="The Last Kingdom cover" />
+				</div>
+				<div class="movie">
+					<img src={blackDeath} alt="Black Death cover" />
+				</div>
+				<div class="movie">
+					<img src={ipMan} alt="Ip Man cover" />
+				</div>
 			</section>
 		</div>
   	);
