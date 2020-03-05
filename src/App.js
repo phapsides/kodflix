6daa9d6f0b5211dc191c9a09463a2ae67943d560
Panos Hapsides <p.hapsides@live.com>
2020-03-05 14:08:25 +0000

    Move images in movie component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
-import Movie from './Movie/Movie.js';
+import Movie from './Movie/Movie';
 import './App.css';
 import kodflixLogo from './images/kodflix-logo.png';
-import lastSamurai from './images/covers/the-last-samurai.jpg';
-import troy	from './images/covers/troy.jpg';
-import marcoPolo from './images/covers/marco-polo.jpg';
-import theLastKingdom from './images/covers/the-last-kingdom.jpg';
-import blackDeath from './images/covers/black-death.jpg';
-import ipMan from './images/covers/ip-man.jpg';
+import lastSamurai from './Movie/covers/the-last-samurai.jpg';
+import troy	from './Movie/covers/troy.jpg';
+import marcoPolo from './Movie/covers/marco-polo.jpg';
+import theLastKingdom from './Movie/covers/the-last-kingdom.jpg';
+import blackDeath from './Movie/covers/black-death.jpg';
+import ipMan from './Movie/covers/ip-man.jpg';
 
 function App() {
   	return (
