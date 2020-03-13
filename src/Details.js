8ed9d9e2cce719549c79dd15afaf0c27a6485465
Panos Hapsides <p.hapsides@live.com>
2020-03-13 11:39:41 +0000

    Migrate from stateless to stateful component

--- src/Details.js
+++ src/Details.js
@@ -1,11 +1,13 @@
-import React from 'react';
+import React, { Component} from 'react';
 import { Link } from 'react-router-dom';
 
-export default function Details() {
-	return (
-		<section>
-			<h1>Hello, this will be the details page for each Movie & TV show :)</h1>
-			<Link to='/'> &#60; button </Link>
-	 	</section>
-	);
+export default class Details extends Component {
+	render() {
+		return (
+			<section>
+				<h1>Hello, this will be the details page for each Movie & TV show.</h1>
+				<Link to='/'> &#60; button </Link>
+			</section>
+		)
+	}
 }
