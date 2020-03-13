a89030a2dadf03ead1676c67c81cf132174a8556
Panos Hapsides <p.hapsides@live.com>
2020-03-13 11:43:23 +0000

    Introduce new state after a delay

--- src/Details.js
+++ src/Details.js
@@ -10,6 +10,14 @@ export default class Details extends Component {
 		}
 	}
 
+	componentDidMount() {
+		setTimeout(() => {
+			this.setState({
+				message: 'Coming soon!'
+			});
+		}, 3000);
+	}
+
 	render() {
 		return (
 			<section>
