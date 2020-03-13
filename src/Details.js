39ba7ee3d57bd7aa7225d3135ecab4a4cf99b9ff
phapsides <p.hapsides@live.com>
2020-03-13 13:17:49 +0000

    Introduce component state #13 (#1)
    * Migrate from stateless to stateful component
    
    * Create and display a new state property
    
    * Introduce new state after a delay

--- src/Details.js
+++ src/Details.js
@@ -1,11 +1,29 @@
-import React from 'react';
+import React, { Component} from 'react';
 import { Link } from 'react-router-dom';
 
-export default function Details() {
-	return (
-		<section>
-			<h1>Hello, this will be the details page for each Movie & TV show :)</h1>
-			<Link to='/'> &#60; button </Link>
-	 	</section>
-	);
+export default class Details extends Component {
+
+	constructor() {
+		super();
+		this.state = {
+			message: 'Hello, this will be the details page for each Movie & TV show.'
+		}
+	}
+
+	componentDidMount() {
+		setTimeout(() => {
+			this.setState({
+				message: 'Coming soon!'
+			});
+		}, 3000);
+	}
+
+	render() {
+		return (
+			<section>
+				<h1>{this.state.message}</h1>
+				<Link to='/'> &#60; button </Link>
+			</section>
+		)
+	}
 }
