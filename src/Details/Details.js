23c29b09ae49e8169a4e7664dd888a2ea1c2f7ce
Panos Hapsides <p.hapsides@live.com>
2020-03-17 10:51:54 +0000

    Move details to it's own component

---
+++ src/Details/Details.js
@@ -0,0 +1,44 @@
+import React, { Component} from 'react';
+import { Redirect } from 'react-router-dom';
+import getMovies from './../Gallery/getMovies';
+import './Details.scss';
+
+
+export default class Details extends Component {
+
+	constructor() {
+		super();
+		this.state = {
+			movie: {}
+		}
+	}
+
+	componentDidMount() {
+		let movieSlug = this.props.match.params.slug;
+		let movieObject = getMovies().find(function (movie) {
+			return movie.slug === movieSlug;
+		});
+
+		this.setState({
+			movie: movieObject
+		});
+	}
+
+	render() {
+		if(this.state.movie === undefined) {
+			return (
+				<Redirect to='/not-found' />
+			)
+		} else {
+			return (
+				<main className="Details">
+					<img src={this.state.movie.image} alt={this.state.movie.imageAlt} />
+					<div className="data">
+						<h1>{this.state.movie.title}</h1>
+						<p>{this.state.movie.synopsis}</p>
+					</div>
+				</main>
+			)
+		}
+	}
+}
