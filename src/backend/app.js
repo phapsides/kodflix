57fd7b4ee5e203a7c513c98192b90850a0b7b80a
Panos Hapsides <p.hapsides@live.com>
2020-03-18 10:52:46 +0000

    Return a list of shows from the backend #19

--- src/backend/app.js
+++ src/backend/app.js
@@ -1,7 +1,9 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
+const port = 3001;
 
-app.get('/', (req, res) => res.send('Hello World!'))
+const getShows = require('./shows');
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.get('/rest/shows', (req, res) => res.send(getShows()));
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
---
+++ src/backend/shows.js
@@ -0,0 +1,34 @@
+module.exports = function getShows() {
+    return [
+        {
+            "slug": "the-last-samurai",
+            "title": "The Last Samurai",
+            "synopsis": "Captain Nathan Algren is hired to train the Japanese army to fight a Samurai rebellion. He, however, ends up embracing the same Samurai culture which he was assigned to destroy."
+        },
+        {
+            "slug": "troy",
+            "title": "Troy",
+            "synopsis": "After Paris, a prince of Troy, has an affair with Menelaus's wife, Helen, he decides to take her with him. Later, Menelaus's brother uses that as an excuse to wage war against the city of Troy."
+        },
+        {
+            "slug": "marco-polo",
+            "title": "Marco Polo",
+            "synopsis": "Venetian merchant and adventurer Marco Polo journeys across Europe and Asia and lands up as a prisoner in the palace of Kublai Khan, a 13th century Mongolian emperor."
+        },
+        {
+            "slug": "the-last-kingdom",
+            "title": "The Last Kingdom",
+            "synopsis": "During their invasion of England, the Danes capture Uhtred, a young successor of Saxon earldom, and raise him as their own. Years later, Uhtred's loyalties are put to the test by the Danes.
+        },
+        {
+            "slug": "black-death",
+            "title": "Black Death",
+            "synopsis": "When the bubonic plague breaks out in the city of England, killing hundreds of people, a monk decides to investigate a report, which states that some people are miraculously coming back to life."
+        },
+        {
+            "slug": "ip-man",
+            "title": "Ip Man",
+            "synopsis": "The life story of Yip Man, the first person to teach the Chinese martial art of Wing Chun and Bruce Lee's trainer."
+        }
+    ]
+}
