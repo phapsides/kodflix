d4c591b9ebfe2f1a01a5235b69f21d05c29fc00a
Panos Hapsides <p.hapsides@live.com>
2020-03-23 15:00:17 +0000

    Enable the backend server in prod #23

--- src/backend/app.js
+++ src/backend/app.js
@@ -1,13 +1,15 @@
 const express = require('express');
+const path = require('path');
 const app = express();
-const port = process.env.PORT || 3001;
 
+const port = process.env.PORT || 3001;
 const getShows = require('./shows');
+
+
+app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname, './../../build', 'index.html'));
+});
 
 app.get('/rest/shows', (req, res) => res.send(getShows()));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
-
-app.get('*', function (req, res) {
-    res.sendFile(path.join(__dirname, './../../build', 'index.html'));
-});
