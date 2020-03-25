b843f29f206695f51ad461861a08237a01e7b863
Panos Hapsides <p.hapsides@live.com>
2020-03-25 14:21:32 +0000

    Add image file name to data stacture

--- src/backend/shows.js
+++ src/backend/shows.js
@@ -1,32 +1,38 @@
 module.exports = function getShows() {
     return [
-        {
+        {   
             "slug": "the-last-samurai",
+            "image": "the-last-samurai.jpg",
             "title": "The Last Samurai",
             "synopsis": "Captain Nathan Algren is hired to train the Japanese army to fight a Samurai rebellion. He, however, ends up embracing the same Samurai culture which he was assigned to destroy."
         },
         {
             "slug": "troy",
+            "image": "troy.jpg",
             "title": "Troy",
             "synopsis": "After Paris, a prince of Troy, has an affair with Menelaus's wife, Helen, he decides to take her with him. Later, Menelaus's brother uses that as an excuse to wage war against the city of Troy."
         },
         {
             "slug": "marco-polo",
+            "image": "marco-polo.jpg",
             "title": "Marco Polo",
             "synopsis": "Venetian merchant and adventurer Marco Polo journeys across Europe and Asia and lands up as a prisoner in the palace of Kublai Khan, a 13th century Mongolian emperor."
         },
         {
             "slug": "the-last-kingdom",
+            "image": "the-last-kingdom.jpg",
             "title": "The Last Kingdom",
             "synopsis": "During their invasion of England, the Danes capture Uhtred, a young successor of Saxon earldom, and raise him as their own. Years later, Uhtred's loyalties are put to the test by the Danes."
         },
         {
             "slug": "black-death",
+            "image": "black-death.jpg",
             "title": "Black Death",
             "synopsis": "When the bubonic plague breaks out in the city of England, killing hundreds of people, a monk decides to investigate a report, which states that some people are miraculously coming back to life."
         },
         {
             "slug": "ip-man",
+            "image": "ip-man.jpg",
             "title": "Ip Man",
             "synopsis": "The life story of Yip Man, the first person to teach the Chinese martial art of Wing Chun and Bruce Lee's trainer."
         }
