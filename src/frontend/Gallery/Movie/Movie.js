6468c3328620bc357f84e6d730cc2fba17f1ff3c
Panos Hapsides <p.hapsides@live.com>
2020-03-25 14:22:02 +0000

    Update image reference

--- src/frontend/Gallery/Movie/Movie.js
+++ src/frontend/Gallery/Movie/Movie.js
@@ -6,7 +6,7 @@ import iconStar from './../assets/star.svg';
 export default function Movie(props) {
 	return (
 		<Link to={`/${props.slug}`} className="Movie">
-			<img src={props.image} alt={`${props.imageAlt} cover`} />
+			<img src={require(`./../../common/images/${props.image}`)} alt={`${props.imageAlt} cover`} />
 			<section className="data">
 				<h1 className="movieTitle">{props.title}</h1>
 				<p>Rating: <img className="icon-star" src={iconStar} alt="Rating star" />{props.rating}/10</p>
